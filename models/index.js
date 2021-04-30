f11836a3e49f2f511a7cbb48d6f05da4810801de
Savannah Bennett <savvy.bennett8@gmail.com>
2021-04-29 19:34:48 -0600

    index and post model setup

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const Post = require("./Post");
+
+//create associations
+// User.hasMany(Post, {
+//     foreignKey: 'user_id'
+// });
+
+// Post.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
+
+// module.exports = Post;
