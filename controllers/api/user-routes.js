a2ab1bf967382a653dd15bf53f7020fdb77f8d0b
Yadada <u1068120@utah.edu>
2021-05-04 21:30:34 -0600

    api route for login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -51,4 +51,54 @@ router.get("/:id", (req, res) => {
         console.log(err)
         res.status(500).json(err)
     });
-} )
+} );
+
+//POST/api/users/
+router.post("/", (req, res) => {
+    User.create({
+        username: req.body.username, 
+        email: req.body.email,
+        password: req.body.password,
+
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        })
+    })
+});
+
+//POST/api/users/login
+router.post("/login", (req, res) =>{
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({message: "Nosuer withthat email address!"});
+            return;
+        }
+
+        //Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!'});
+            return;
+        }
+
+        req.session.save(() => {
+            //declare session variable
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData,message: 'You are now logged in!'});
+        });
+    });
+});
