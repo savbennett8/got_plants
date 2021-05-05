cd44d17d397640dada961805fd1c7a7b485fcc27
Yadada <u1068120@utah.edu>
2021-05-04 20:58:01 -0600

    api route for users id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,3 +14,41 @@ router.get("/", (req, res) => {
         res.status(500).json(err)
     });
 });
+
+//GET/api/Users/:id
+router.get("/:id", (req, res) => {
+    User.findOne({
+        where:{
+            id:req.params.id
+        },
+        attributes:{exclude: ["password"]},
+        include:[
+            {
+                model:Post, 
+                attributes:["id", "title", "post_body", "post_url", "created_at"]
+            },
+            {
+               model:Comment,
+               attributes:["id", "comment_body", "created_at"],
+               include:{
+                   model:Post, 
+                   attributes:["title"]
+               }
+            },
+        ]
+       
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({
+                message:"No user found whith this id."
+            });
+            return;
+        }
+        return res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    });
+} )
