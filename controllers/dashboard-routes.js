12e0057d2fbd4816c0d27057df4726de859f287f
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-09 20:40:59 -0600

    edit form from dashboard works

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -35,4 +35,46 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/edit/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'post_body', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('edit-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
 module.exports = router;
